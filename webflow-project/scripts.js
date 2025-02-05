b6cf861c9d55bce59236dc302b9350219f389efb
Zenpixel007 <josh@zenpixel.co.uk>
2025-02-05 12:09:33 +0000

    Initial commit - adding console.log

--- webflow-project/scripts.js
+++ webflow-project/scripts.js
@@ -2,6 +2,7 @@
 // Your custom code for animations, cursor, and Barba transitions:
 
 //This is a test to see if it is conected to the webflow project
+console.log("Custom JS loaded");
 
 // Hero Animations GSAP
 let tl = gsap.timeline();
