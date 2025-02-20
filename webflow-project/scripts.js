dd3aadad1f211388f0695150c7140c0fb804b347
Zenpixel007 <josh@zenpixel.co.uk>
2025-02-20 13:16:28 +0000

    GSAP hero animation completed

--- webflow-project/scripts.js
+++ webflow-project/scripts.js
@@ -2,7 +2,7 @@
 // Your custom code for animations, cursor, and Barba transitions:
 
 //This is a test to see if it is conected to the webflow project
-console.log("test");
+console.log("latest version");
 
 // Function to reinitialize Calendly
 function initCalendly() {
