a6e1cb4d4b354562bc21196e525b60ff5879528b
Zenpixel007 <josh@zenpixel.co.uk>
2025-02-22 09:43:11 +0000

    Fine-tune hero section animation timing offset

--- webflow-project/scripts.js
+++ webflow-project/scripts.js
@@ -198,7 +198,7 @@ function initGsapAnimations() {
       scale: 1.1,
       duration: 0.3,
       ease: "power2.inOut"
-    }, "-=0.2");
+    }, "-=0.5");
 
   // Footer Animation
   gsap.to(".main-wrapper", {
