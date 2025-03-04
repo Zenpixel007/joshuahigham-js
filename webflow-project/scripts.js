c8be55f9d73de51695a60eb9b15c1691a6fb9702
Zenpixel007 <josh@zenpixel.co.uk>
2025-03-04 13:42:03 +0000

    Update Rive layout to use 'fill' instead of 'cover' for better canvas rendering

--- webflow-project/scripts.js
+++ webflow-project/scripts.js
@@ -30,7 +30,7 @@ async function initRive() {
       stateMachines: ['BlobFollow'],
       autoplay: true,
       layout: new rive.Layout({
-        fit: rive.Fit.cover,
+        fit: rive.Fit.fill,
         alignment: rive.Alignment.center,
       }),
       onLoad: () => {
@@ -47,7 +47,7 @@ async function initRive() {
         canvas.width = container.offsetWidth;
         canvas.height = container.offsetHeight;
         riveInstance.layout = new rive.Layout({
-          fit: rive.Fit.cover,
+          fit: rive.Fit.fill,
           alignment: rive.Alignment.center,
         });
       }
