c983682f098c739c521d474d6c7159a07afd821b
Zenpixel007 <josh@zenpixel.co.uk>
2025-03-06 11:18:35 +0000

    Update Rive animation URL to v9 responsive hero animation

--- webflow-project/scripts.js
+++ webflow-project/scripts.js
@@ -33,7 +33,7 @@ async function initRive() {
     // Initial size setup
     updateCanvasSize();
 
-    const riveURL = 'https://cdn.prod.website-files.com/67a1da359110aff234167390/67c86dbc2ea403f36d285cb5_responsive-hero-animation-v8.riv';
+    const riveURL = 'https://cdn.prod.website-files.com/67a1da359110aff234167390/67c98460450dabd44988e3cc_responsive-hero-animation-v9.riv';
     
     // Function to determine which artboard to use
     const getArtboardConfig = () => {
